d95d2782c29c1b60f7e4022ca3a0b9c81607f2fc
Samuel Davidson <samueldavidson@google.com>
2019-10-25 13:38:25 -0700

    Fixed rotation age logic.

--- staging/src/k8s.io/client-go/plugin/pkg/client/auth/exec/exec.go
+++ staging/src/k8s.io/client-go/plugin/pkg/client/auth/exec/exec.go
@@ -354,9 +354,13 @@ func (a *Authenticator) refreshCredsLocked(r *clientauthentication.Response) err
 	a.cachedCreds = newCreds
 	// Only close all connections when TLS cert rotates. Token rotation doesn't
 	// need the extra noise.
-	if a.onRotate != nil && oldCreds != nil && !reflect.DeepEqual(oldCreds.cert, a.cachedCreds.cert) {
-		metrics.ClientCertRotationAge.Observe(oldCreds.cert.Leaf.NotAfter.Sub(oldCreds.cert.Leaf.NotBefore))
-		a.onRotate()
+	if oldCreds != nil && !reflect.DeepEqual(oldCreds.cert, a.cachedCreds.cert) {
+		if oldCreds.cert != nil { // Can be nil if the exec auth plugin only returned token auth.
+			metrics.ClientCertRotationAge.Observe(time.Now().Sub(oldCreds.cert.Leaf.NotBefore))
+		}
+		if a.onRotate != nil {
+			a.onRotate()
+		}
 	}
 	metrics.ClientCertExpiration.Set(newCreds.cert.Leaf.NotAfter)
 	return nil
